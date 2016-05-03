47e252f1b6ed0c75e5b066b64ee53ed5d3d59105
Nitin Gupta <gniting@users.noreply.github.com>
2016-05-03 13:42:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`Hmmm, I didn't understand that.`).then(() => 'speak');
+                    return bot.say(`Hmmm, I didn't understand that.\nTry typing in BOT to get me going the right way. %[BOT](postback:bot)`).then(() => 'speak');
                 }
 
                 var response = scriptRules[upperText];
